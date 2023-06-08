c058c88eea837497a1afe56bdefe992c84f31ba5
Gathsara2002 <liviru2002mmg@gmail.com>
2023-06-08 17:03:08 +0530

    create server ui

---
+++ src/controller/ServerController.java
@@ -0,0 +1,23 @@
+/**
+ * @author : Gathsara
+ * created : 6/6/2023 -- 8:31 PM
+ **/
+
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class ServerController {
+    public AnchorPane serverAP;
+    public TextArea txtChat;
+    public TextField txtMsgField;
+
+    public void txtMsg(ActionEvent actionEvent) {
+    }
+
+    public void btnSendOnAction(ActionEvent actionEvent) {
+    }
+}
