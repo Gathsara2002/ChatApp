bafa5e3b469ca3cc550bc45b3489387ab748bbb2
Gathsara2002 <liviru2002mmg@gmail.com>
2023-06-08 17:18:11 +0530

    create client 2 ui and initializer

---
+++ src/controller/Client2Controller.java
@@ -0,0 +1,20 @@
+/**
+ * @author : Gathsara
+ * created : 6/8/2023 -- 5:15 PM
+ **/
+
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class Client2Controller {
+    public AnchorPane client2AP;
+    public TextField txtMsgField;
+    public TextArea txtChat;
+
+    public void btnSendOnAction(ActionEvent actionEvent) {
+    }
+}
