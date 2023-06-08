4debe96c976837ae6364111c2cfcb4f84eaaff3b
Gathsara2002 <liviru2002mmg@gmail.com>
2023-06-08 18:31:25 +0530

    create communication between 1 st client

--- src/controller/ServerController.java
+++ src/controller/ServerController.java
@@ -40,6 +40,12 @@ public class ServerController {
     final int Port2 = 6001;
     final int Port3 = 6002;
 
+    /*get massage from clients*/
+    String massage1, massage2, massage3;
+
+    /*store client's massages separately*/
+    String chat1, chat2, chat3;
+
     public void txtMsg(ActionEvent actionEvent) {
     }
 
@@ -60,6 +66,30 @@ public class ServerController {
                 txtChat.appendText("Server is started...\n");
                 localSocket1 = serverSocket.accept();
                 txtChat.appendText("Client 1  accepted");
+
+                /* accepting input and output streams */
+                dataInputStream1 = new DataInputStream(localSocket1.getInputStream());
+                dataOutputStream1 = new DataOutputStream(localSocket1.getOutputStream());
+
+                while (!massage1.equalsIgnoreCase("exit")) {
+
+                    /*get client 1 massage to server*/
+                    massage1 = dataInputStream1.readUTF();
+                    txtChat.appendText("\n Client 1 : " + massage1.trim());
+
+                    /*store massage to another variable*/
+                    chat1 = massage1;
+
+                    /*send massage to client 2*/
+                    dataOutputStream2.writeUTF("\n client 1 : " + chat1.trim());
+                    dataOutputStream2.flush();
+
+                    /*send massage to client 3*/
+                    dataOutputStream3.writeUTF("\n client 1 : " + chat1.trim());
+                    dataOutputStream3.flush();
+
+                }
+
 
             } catch (IOException e) {
                 e.printStackTrace();
